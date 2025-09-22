f30f5a1f5ad147ba316f5c6f93faa516227588b0
MessP13 <messigameplays24@gmail.com>
2025-09-22 19:58:26 +0200

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,14 +3,14 @@ import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
 
 if (!process.env.GOOGLE_GENAI_API_KEY) {
-  throw new Error('A variável de ambiente GOOGLE_GENAI_API_KEY não está definida.');
+  throw new Error("A variável de ambiente GOOGLE_GENAI_API_KEY não está definida.");
 }
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY, // pega do .env
+      apiKey: process.env.GOOGLE_GENAI_API_KEY,
     }),
   ],
-  model: 'googleai/gemini-2.5-flash', // string correta
+  model: 'googleai/gemini-2.5-flash', // usar string
 });
